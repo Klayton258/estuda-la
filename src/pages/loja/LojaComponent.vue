f82b53e67802e9122db52447f66fcdc08702aad8
Klayton Massango <klayton0304massango@gmail.com>
2022-06-22 20:57:34 +0200

    store update

--- src/pages/loja/LojaComponent.vue
+++ src/pages/loja/LojaComponent.vue
@@ -1,18 +1,37 @@
 <template>
   <navbar />
   <section class="header">
-    <div class="container">
+    <div class="contain">
      <img src="images/loja-banner.png" width="100%"  alt="">
     </div>
   </section>
   <section class="content">
 
-    <div class="container">
-      <aside class="col-4">
-        <!-- <h1>Livros Populares</h1> -->
+    <div class="row popular-row">
+      <aside class="col-lg-4">
+        <h1>Livros Populares</h1>
+        <div class="row">
+          <img src="images/books/1-1.png" alt="">
+        </div>
       </aside>
-      <article class="col-8">
-        
+      <article class="col-lg-8">
+        <div class="col-8 col-btns">
+          <div class="row levels-school g-4">
+            <div class="col-2 btn-class">
+              <div class="btn btn-primary btn-cursos active">Todos livros</div>
+            </div>
+            <div class="col-2 btn-class">
+              <div class="btn btn-primary btn-cursos">Creache</div>
+            </div>
+            <div class="col-2 btn-class">
+              <div class="btn btn-primary btn-cursos">Secundaria</div>
+            </div>
+            <div class="col-2 btn-class">
+              <div class="btn btn-primary btn-cursos">Universidade</div>
+            </div>
+          </div>
+        </div>
+
       </article>
     </div>
   </section>
